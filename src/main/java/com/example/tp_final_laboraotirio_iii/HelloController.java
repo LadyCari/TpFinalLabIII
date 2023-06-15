a07683d9fd044d31820373a7dd4d1150e6d595a0
LadyCari <k_roferraris@hotmail.com>
2023-06-15 18:16:25 -0300

    programa en blanco

---
+++ src/main/java/com/example/tp_final_laboraotirio_iii/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.tp_final_laboraotirio_iii;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.tp_final_laboraotirio_iii {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.tp_final_laboraotirio_iii to javafx.fxml;
+    exports com.example.tp_final_laboraotirio_iii;
+}
