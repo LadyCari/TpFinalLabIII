fea10cfaf03d44f86751a53008451ff5bcdb2410
LadyCari <k_roferraris@hotmail.com>
2023-06-15 22:28:00 -0300

    cambioEscenarioParaNoRomper

--- src/main/java/com/example/tp_final_laboraotirio_iii/Main.java
+++ src/main/java/com/example/tp_final_laboraotirio_iii/Main.java
@@ -10,9 +10,8 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MaleniaGurciaTPfinal.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setScene(scene);
         stage.show();
     }
