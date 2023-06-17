48b430a0f1db8fb60587cf6af2b3b8fc6d651b00
Santiago <rubinisantiago@hotmail.com>
2023-06-17 20:44:10 -0300

    Principales imagenes funcionando.

--- src/main/java/com/example/tp_final_laboraotirio_iii/Main.java
+++ src/main/java/com/example/tp_final_laboraotirio_iii/Main.java
@@ -2,10 +2,14 @@ package com.example.tp_final_laboraotirio_iii;
 
 import com.example.tp_final_laboraotirio_iii.Modelos.GameData;
 import com.example.tp_final_laboraotirio_iii.Modelos.Personaje;
+import com.example.tp_final_laboraotirio_iii.Repositorio.PersonajeRepo;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Control;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -29,8 +33,37 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch();
 
+        //region test
+        PersonajeRepo personajeRepo = new PersonajeRepo();
 
+        Personaje personaje = new Personaje("Santurro03");
 
+        GameData gameData = new GameData();
+
+        gameData.setAsistenciaClase(GameData.AsistenciaClase.PRESENTE);
+
+        gameData.setEventoCompletado(GameData.eventoCompletado.COMPLETADO);
+
+        gameData.setFecha(1);
+
+        GameData gameData2 = new GameData();
+
+        gameData2.setAsistenciaClase(GameData.AsistenciaClase.PRESENTE);
+
+        gameData2.setEventoCompletado(GameData.eventoCompletado.COMPLETADO);
+
+        gameData2.setFecha(2);
+
+        ArrayList<GameData> listaGuardados = personaje.getGuardadoPartida();
+
+        listaGuardados.add(gameData);
+        listaGuardados.add(gameData2);
+
+        personajeRepo.Agregar(personaje);
+
+        System.out.println(personaje);
+        //endregion
 
     }
+
 }
--- src/main/java/com/example/tp_final_laboraotirio_iii/SceneController.java
+++ src/main/java/com/example/tp_final_laboraotirio_iii/SceneController.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -85,6 +86,18 @@ public class SceneController {
 
     public void switchToMaleniaGurciaTPfinal(ActionEvent event) {
         try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/example/tp_final_laboraotirio_iii/MaleniaGurciaTPfinal.fxml")));
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void switchToMaleniaGurciaNormal(ActionEvent event) {
+        try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/example/tp_final_laboraotirio_iii/ClaseMaleniaGurcia.fxml")));
             stage = (Stage)((Node)event.getSource()).getScene().getWindow();
             scene = new Scene(root);
@@ -109,4 +122,16 @@ public class SceneController {
             System.out.println("Error: El stage es nulo. Asegúrate de haber asignado el stage correctamente en el controlador.");
         }
     }
+
+    public void handleButtonClick(ActionEvent event) {
+        // Aquí va tu lógica para determinar el contexto
+        boolean isConditionMet = false;
+
+        if (isConditionMet) {
+            switchToMaleniaGurciaNormal(event);
+        } else {
+            switchToMaleniaGurciaTPfinal(event);
+        }
+    }
+
 }
